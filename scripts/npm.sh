b9e0b67ff5aea69d5ba4b726a3dd83f51c8a2710
Adonis K <aklp08@gmail.com>
2018-05-23 15:54:53 +0200

    Update npm.sh

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -1,31 +1,18 @@
 #!/usr/bin/env bash
 
 packages=(
-	babel-cli
-	autoprefixer
-	bower
-	browser-sync
-	browserify
-	clean-css
-	coffeelint
 	csshint
 	csslint
 	eslint
 	gulp
-	grunt-cli
-	grunt-generate-configs
-	grunt-init
 	jasmine
 	eslint
 	jsonlint
 	jsxhint
 	karma-cli
-	less
-	load-grunt-configs
 	localtunnel
 	nodemon
 	npm-check-updates
-	parker
 	pm2
 	prettyjson
 	sassdoc
